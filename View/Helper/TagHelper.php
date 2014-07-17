556fbae305a8ea24b55e4287ade7655d7fe4dc58
Wes Sonnenreich <wes@sonnenreich.com>
2014-07-17 11:36:58 +1000

    fixes

--- View/Helper/TagHelper.php
+++ View/Helper/TagHelper.php
@@ -15,44 +15,26 @@ class TagHelper extends AppHelper {
 		'tag_placeholder' => 'search/add tags...',
 		'tag_hidden' => false,
 		'tag_width' => '90%',
-		'tag_autocomplete' =>  false,
-		'tag_data' => [],
-		'tag_maxselect' => null
 	);
 
-	public function input($field, $identifier, $options=array()) {
+	public function input($field, $namespace, $options=array()) {
 		$options = am($this->options, $options);
 
-		$this->Html->css("bootstrap-tagsinput.css", array('block' => 'css'));
-		$this->Html->script("bootstrap-tagsinput.min", array('block' => 'script'));
+		$this->Html->script("bootstrap-tag.min", array('block' => 'script'));
 		$this->Html->script("Tags.taginput", array('block' => 'script'));
 
-		$formopts = [
-			'type' => 'hidden',
-			'class' => 'select2tags',
-			'multiple' => true,
-			'label' => false,
-			'legend' => false,
-			'div' => false,
-			'tags' => json_encode($options['tag_data']),
-			'placeholder' => $options['tag_placeholder'],
-			'data-placeholder' => $options['tag_placeholder'],
-			'identifier' => $identifier,
-			'maxselect' => $options['tag_maxselect']
-		];
-		if ($options['tag_autocomplete']) {
-			$formopts['url'] = $this->Html->url(array('action'=>'autocomplete_tags')) . ".json";
-		}
-		if ($options['tag_width']) $formopts['style'] = "width: {$options['tag_width']}";
-
-		echo $this->Form->input($field, $formopts);
+		echo $this->Form->input($field, array('type' => 'text', 'label' => false, 'legend' => false, 'div' => false, 'data-provide'=>'tag', 'placeholder' => $options['tag_placeholder'], 'data-placeholder' => $options['tag_placeholder'], 'url' => $this->Html->url(array('plugin' => 'storage', 'admin' => false, 'controller' => 'filestores', 'action'=>'autocomplete_tags')) . ".json", 'namespace' => $namespace));
 
 		$far = explode('.',$field);
 		$tagid = "#";
 		foreach ($far as $f) {
 			$tagid .= ucfirst($f);
 		}
-		$this->Js->buffer("handleTags('$tagid');");
+		$this->Js->buffer("
+			handleTags('$tagid');
+			$('.tags').attr('style', 'width: {$options['tag_width']}');
+
+		");
 	}
 
 }
